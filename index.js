6cf738272e45d2248e6992c2aa661e03ec6b4b3e
k3v3ns <72592651+k3v3ns@users.noreply.github.com>
2021-12-17 13:03:59 -0700

    mod: index line9

--- index.js
+++ index.js
@@ -6,16 +6,17 @@ const app = express()
 
 app.use(cors())
 
-app.get('/inicio', (req, res) => {
+app.get('https://servicio-estetica22.herokuapp.com', (req, res) => {
     res.send('Hola desde tu primera ruta de la API')
 })
 
 const credential = {
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'veterinaria_estetica'
+    host: 'us-cdbr-east-05.cleardb.net',
+    user: 'b65f8b2aefe726',
+    password: '11af13b9',
+    database: 'heroku_b0651ecedbcc6b1'
 }
+//mysql://b65f8b2aefe726:11af13b9@us-cdbr-east-05.cleardb.net/heroku_b0651ecedbcc6b1?reconnect=true
 
 app.get('/usuarios', (req, res) =>{
     var connection = mysql.createConnection(credential)
